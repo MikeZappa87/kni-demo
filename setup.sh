c4a9f924de0be2266f303f434de3105a71677e46
Michael Zappa <michael.zappa@gmail.com>
2023-12-20 22:00:22 -0700

    initial commit

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+kind delete cluster --name test1 || true
+
+kind create cluster --config kind.yaml --name test1
+
+kubectl cluster-info --context kind-test1
+
+kubectl scale --replicas=0 -n kube-system deployment coredns
+kubectl scale --replicas=0 -n local-path-storage deployment local-path-provisioner
+
+kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=5m
+
+sleep 20
+
+docker cp ./bin/containerd test1-control-plane:/usr/local/bin/containerd
+docker cp ./bin/server test1-control-plane:/usr/bin/server
+docker cp ./bin/kubelet test1-control-plane:/usr/bin/kubelet
+docker cp ./kni.service test1-control-plane:/etc/systemd/system/kni.service
+
+docker exec test1-control-plane systemctl daemon-reload
+docker exec test1-control-plane systemctl start kni
+docker exec test1-control-plane systemctl restart kubelet
+docker exec test1-control-plane systemctl restart containerd
+
+kubectl scale --replicas=2 -n kube-system deployment coredns
+kubectl scale --replicas=1 -n local-path-storage deployment local-path-provisioner
+
+kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=5m
+
+kubectl get pods -A
