cf3eed636f1b43cc8db086c835241c6d06538039
Michael Zappa <michael.zappa@gmail.com>
2023-12-21 22:57:51 -0700

    update dev

--- setup.sh
+++ setup.sh
@@ -19,11 +19,23 @@ docker cp ./bin/containerd test1-control-plane:/usr/local/bin/containerd
 docker cp ./bin/server test1-control-plane:/usr/bin/server
 docker cp ./bin/kubelet test1-control-plane:/usr/bin/kubelet
 docker cp ./kni.service test1-control-plane:/etc/systemd/system/kni.service
+docker cp ./config.toml test1-control-plane:/etc/containerd/config.toml
 
 docker exec test1-control-plane systemctl daemon-reload
 docker exec test1-control-plane systemctl start kni
 docker exec test1-control-plane systemctl restart kubelet
 docker exec test1-control-plane systemctl restart containerd
+
+docker cp ./bin/containerd test1-worker:/usr/local/bin/containerd
+docker cp ./bin/server test1-worker:/usr/bin/server
+docker cp ./bin/kubelet test1-worker:/usr/bin/kubelet
+docker cp ./kni.service test1-worker:/etc/systemd/system/kni.service
+docker cp ./config.toml test1-worker:/etc/containerd/config.toml
+
+docker exec test1-worker systemctl daemon-reload
+docker exec test1-worker systemctl start kni
+docker exec test1-worker systemctl restart kubelet
+docker exec test1-worker systemctl restart containerd
 
 kubectl scale --replicas=2 -n kube-system deployment coredns
 kubectl scale --replicas=1 -n local-path-storage deployment local-path-provisioner
